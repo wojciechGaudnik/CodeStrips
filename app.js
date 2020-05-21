8502ab0b4d6336372ec077b097804c62df51894a
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-05-21 17:50:04 +0200

    added chai mocha

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const jsonParser = bodyParser.json();
 
 const PORT = process.env.PORT || 4001;
 
-// todo https://www.youtube.com/watch?v=7ITfaw7BRKk&feature=youtu.be
-
-
 app.get('/', (req,res) => {
     res.send('hello world')
 })
